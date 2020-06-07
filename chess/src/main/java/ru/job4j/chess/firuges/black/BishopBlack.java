0ea740292ef1aec345ce8d4b1231bca4b6704195
slash25 <jamaslash@gmail.com>
2020-06-07 16:17:40 +0300

    test position, copy, move work

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -21,32 +21,107 @@ public class BishopBlack implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
+    public  Cell[] way(Cell source, Cell dest) {
+
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not way by diagonal from %s to %s", source, dest)
+            );
+        }
+
+        //написать метод проверки в какую сторону диагональ
+        int size = Math.abs(dest.x - source.x) + 1;
+        Cell[] steps = new Cell[size];
+        switch (findDirection(source, dest)) {
+
+            //Вверх влево
+            case 1:
+                int deltaXupLeft = 0;
+                int deltaYupLeft = 0;
+               // steps[0] = Cell.findBy(source.x, source.y);
+
+                for (int index = 0; index < size; index++) {
+                    steps[index] = Cell.findBy(source.x + deltaXupLeft, source.y + deltaYupLeft);
+                deltaXupLeft--;
+                deltaYupLeft++;
+                }
+                break;
+
+
+            //Вниз влево
+            case 2:
+                int deltaXdownLeft = 0;
+                int deltaYdownLeft = 0;
+
+                for (int index = 0; index < size; index++) {
+                    steps[index] = Cell.findBy(source.x + deltaXdownLeft, source.y + deltaYdownLeft);
+                    deltaXdownLeft--;
+                    deltaYdownLeft--;
+                }
+                break;
+
+            //Вверх вправо
+            case 3:
+                int deltaXupRight = 0;
+                int deltaYupRight = 0;
+
+                for (int index = 0; index < size; index++) {
+                    steps[index] = Cell.findBy(source.x + deltaXupRight, source.y + deltaYupRight);
+                    deltaXupRight++;
+                    deltaYupRight++;
+                }
+                break;
+
+            //Вниз вправо
+            case 4:
+                int deltaXdownRight = 0;
+                int deltaYdownRight = 0;
+
+                for (int index = 0; index < size; index++) {
+                    steps[index] = Cell.findBy(source.x + deltaXdownRight, source.y + deltaYdownRight);
+                    deltaXdownRight++;
+                    deltaYdownRight--;
+                }
+                break;
+        }
+        return steps;
     }
 
-    public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
-        return false;
+    private int findDirection(Cell source, Cell dest) {
+        int xDir = dest.x - source.x;
+        int yDir = dest.y - source.y;
+
+        //Upp right
+        if ((xDir > 0) && (yDir > 0)) {
+            return 3;
+        }
+
+        //Down right
+        if ((xDir > 0) && (yDir < 0)) {
+            return 4;
+        }
+
+        //Up left
+        if ((xDir < 0) && (yDir < 0)) {
+            return 1;
+        }
+        return 2;//Down left
+    }
+
+    //метод проверки диагонали
+    private boolean isDiagonal(Cell source, Cell dest) {
+        return Math.abs(dest.x - source.x) == Math.abs(dest.y - source.y);
     }
 
     @Override
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
+    }
+
+    @Override
+    public String toString() {
+        return "BishopBlack{" +
+                "position=" + position +
+                '}';
     }
 }
--- chess/src/test/java/ru/job4j/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/BishopBlackTest.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 import java.util.ArrayList;
@@ -19,22 +20,22 @@ public class BishopBlackTest {
 
     @Test
     public void testCopy() {
-        BishopBlack bishop = new BishopBlack(Cell.G1);
-        assertThat(bishop.copy(Cell.G1), is(Cell.G1));
+        BishopBlack bishop = new BishopBlack(Cell.C1);
+        assertThat(bishop.copy(bishop.position()), is(bishop.position()));
     }
 
     @Test
     public void testWay() {
 
         BishopBlack bishopMov = new BishopBlack(Cell.C1);
-        ArrayList<Cell> moving  = new ArrayList<>();
-
-        moving.add(Cell.D2);
-        moving.add(Cell.E3);
-        moving.add(Cell.F4);
-        moving.add(Cell.G5);
-        assertThat(bishopMov.way(Cell.C1, Cell.G5),is(moving));
-
+        Cell[] moving  = {Cell.C1, Cell.D2, Cell.E3, Cell.F4, Cell.G5 };
+        assertThat(bishopMov.way(Cell.C1, Cell.G5), is(moving));
     }
 
+    @Test
+    public void testWayDownRight() {
+        BishopBlack bishopMov = new BishopBlack(Cell.C1);
+        Cell[] moving  = {Cell.D4, Cell.E3, Cell.F2, Cell.G1 };
+        assertThat(bishopMov.way(Cell.D4, Cell.G1), is(moving));
+    }
 }
